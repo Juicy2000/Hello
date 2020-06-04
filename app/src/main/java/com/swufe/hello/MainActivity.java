487e320b30da0bdd9fe40418bc854a9c2e933fe5
Juicy2000 <1076912982@qq.com>
2020-06-04 19:37:26 +0800

    1:GridView显示多栏列表

--- app/src/main/java/com/swufe/hello/MainActivity.java
+++ app/src/main/java/com/swufe/hello/MainActivity.java
@@ -13,86 +13,26 @@ import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.provider.Telephony;
 import android.telecom.Call;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import java.net.ResponseCache;
 
 import javax.security.auth.callback.Callback;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    private class VoteTask extends AsyncTask<String, Void, String> {
 
-        @Override
-        protected String doInBackground(String... params) {
-            for (String p: params ) {
-                Log.i(TAG, "doInBackground: " + p);
-            }
-            String ret = doVote(params[0]);
-            return ret;
+        GridView listView = (GridView) findViewById(R.id.mylist);
+        //init data
+        for(int i=0;i<100;i++){
+            date().add("item" + i);
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-        }
-    }
-    private String doVote(String voteStr){
-        String retStr = "";
-        Log.i("vote", "doVote() voteStr:" + voteStr);
-
-        try {
-
-            StringBuffer stringBuffer = new StringBuffer();        //存储封装好的请求体信息
-            stringBuffer.append("r=").append(URLEncoder.encode(voteStr, "utf-8"));
-
-            byte[] data = stringBuffer.toString().getBytes();
-            String urlPath = "http://192.168.1.102:8080/vote/GetVote";
-            URL url = new URL(urlPath);
-
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-            httpURLConnection.setConnectTimeout(3000);     //设置连接超时时间
-            httpURLConnection.setDoInput(true);                  //打开输入流，以便从服务器获取数据
-            httpURLConnection.setDoOutput(true);                 //打开输出流，以便向服务器提交数据
-            httpURLConnection.setRequestMethod("POST");     //设置以Post方式提交数据
-            httpURLConnection.setUseCaches(false);               //使用Post方式不能使用缓存
-            //设置请求体的类型是文本类型
-            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            //设置请求体的长度
-            httpURLConnection.setRequestProperty("Content-Length", String.valueOf(data.length));
-            //获得输出流，向服务器写入数据
-            OutputStream outputStream = httpURLConnection.getOutputStream();
-            outputStream.write(data);
-
-            int response = httpURLConnection.getResponseCode();            //获得服务器的响应码
-            if(response == HttpURLConnection.HTTP_OK) {
-                InputStream inputStream = httpURLConnection.getInputStream();
-                retStr = inputStreamToString(inputStream);                     //处理服务器的响应结果
-                Log.i("vote", "retStr:" + retStr);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return retStr;
-    }
-}
-    public static String inputStreamToString(InputStream inputStream) {
-        String resultData = null;      //存储处理结果
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        byte[] data = new byte[1024];
-        int len = 0;
-        try {
-            while((len = inputStream.read(data)) != -1) {
-                byteArrayOutputStream.write(data, 0, len);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        resultData = new String(byteArrayOutputStream.toByteArray());
-        return resultData;
-    }
-
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener(this);
+    }}
